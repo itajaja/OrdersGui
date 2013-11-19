53d7ef9892e866c6d174f4e07167b3af840927de
Giacomo Tagliabue <giacomo.tag@gmail.com>
2013-11-18 23:57:03 -0600

    Various Fixes and improvements
    deleted Busy Indicator (not present in toolkit 5, wtf...)
    deleted toolkit 4
    Graphic and functional fixes to loadordermanager

--- OrdersGui/View/LoadOrderManager.xaml.cs
+++ OrdersGui/View/LoadOrderManager.xaml.cs
@@ -17,23 +17,7 @@ namespace Hylasoft.OrdersGui.View
         {
             InitializeComponent();
         }
-
-        private void LoadOrderManager_OnLoaded(object sender, RoutedEventArgs e)
-        {
-            OrdersGrid.Columns[0].Header = Strings_en_us.OrderNumberHeader;
-            OrdersGrid.Columns[1].Header = Strings_en_us.RackHeader;
-            OrdersGrid.Columns[2].Header = Strings_en_us.CustomerHeader;
-            OrdersGrid.Columns[3].Header = Strings_en_us.CarrierHeader;
-            OrdersGrid.Columns[4].Header = Strings_en_us.DateHeader;
-            OrdersGrid.Columns[5].Header = Strings_en_us.TimeHeader;
-            OrdersGrid.Columns[6].Header = Strings_en_us.StartTimeHeader;
-            OrdersGrid.Columns[7].Header = Strings_en_us.EndTimeHeader;
-            OrdersGrid.Columns[8].Header = Strings_en_us.OrderTypeHeader;
-            OrdersGrid.Columns[9].Header = Strings_en_us.DeliveryMethodHeader;
-            OrdersGrid.Columns[10].Header = Strings_en_us.ShipRefHeader;
-            OrdersGrid.Columns[11].Header = Strings_en_us.OrderStatusHeader;
-        }
-
+    
         private void SettingsButton_Click(object sender, RoutedEventArgs e)
         {
             //todo this should be command

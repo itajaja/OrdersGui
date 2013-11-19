a301e64d5b5d9e9bd5888941d0424d5f3a1dec6e
Giacomo Tagliabue <giacomo.tag@gmail.com>
2013-11-19 08:32:34 -0600

    Modified LOD VM
    Added Check Image

--- OrdersGui/ViewModel/LoadOrderDetailsVM.cs
+++ OrdersGui/ViewModel/LoadOrderDetailsVM.cs
@@ -1,14 +1,41 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.Generic;
+using GalaSoft.MvvmLight;
+using Hylasoft.OrdersGui.Model;
+using Hylasoft.OrdersGui.Model.Service;
 
 namespace Hylasoft.OrdersGui.ViewModel
 {
     public sealed class LoadOrderDetailsVM : ViewModelBase
     {
+        private IDataService _dataservice;
+
+        private Order _order;
+        public Order Order
+        {
+            get { return _order; }
+            set { Set("Order", ref _order, value); }
+        }
+
+        private IList<OrderProduct> _orderProducts;
+        public IList<OrderProduct> OrderProducts
+        {
+            get { return _orderProducts; }
+            set { Set("OrderProducts", ref _orderProducts, value); }
+        }
+
+        private IList<OrderCompartment> _orderCompartments;
+        public IList<OrderCompartment> OrderCompartments
+        {
+            get { return _orderCompartments; }
+            set { Set("OrderCompartments", ref _orderCompartments, value); }
+        }
+
         /// <summary>
         /// Initializes a new instance of the LoadOrderDetailsVM class.
         /// </summary>
-        public LoadOrderDetailsVM()
+        public LoadOrderDetailsVM(IDataService ds)
         {
+            _dataservice = ds;
         }
     }
 }

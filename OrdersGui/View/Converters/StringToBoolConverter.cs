ece1b695bade152c73be15fec7ffae3f190c9678
Giacomo Tagliabue <giacomo.tag@gmail.com>
2013-11-23 17:01:52 -0600

    Added ToggleButton Styling
    Added StringToBool Converter

---
+++ OrdersGui/View/Converters/StringToBoolConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace Hylasoft.OrdersGui.View.Converters
+{
+    //todo redesign
+    public class StringToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            List<String> strings = ((string)parameter).Split(',').ToList();
+            return strings.Cast<object>().Contains(value.ToString());
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}

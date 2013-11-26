68923901c626d36088bd001b0fd897ba3718bd00
Giacomo Tagliabue <giacomo.tag@gmail.com>
2013-11-25 22:40:44 -0600

    Created extended DatagridTemplateColumn

---
+++ OrdersGui/Controls/DataGridTemplateColumnEx.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Hylasoft.OrdersGui.Controls
+{
+    public class DataGridTemplateColumnEx : DataGridTextColumn
+    {
+
+        public static readonly DependencyProperty
+            VisibilityDependencyProperty = DependencyProperty.Register(
+                "DVisibility",
+                typeof(Visibility),
+                typeof(DataGridTextColumnEx),
+                new PropertyMetadata(OnDVisibilityChanged));
+
+        public Visibility DVisibility
+        {
+            get { return (Visibility)GetValue(VisibilityDependencyProperty); }
+            set { SetValue(VisibilityDependencyProperty, value); }
+        }
+
+        private static void OnDVisibilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var control = d as DataGridTextColumnEx;
+            var b = (Visibility)e.NewValue;
+            control.Visibility = b;
+        }
+    }
+}
